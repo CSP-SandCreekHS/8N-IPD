3b5abab162633a2745af75bf673e20142c16e5a6
saswanson <saswanson@d49.org>
2018-02-23 14:30:06 -0700

    Team 0 Final

--- team0.py
+++ team0.py
@@ -28,7 +28,7 @@ def move(my_history, their_history, my_score, their_score):
     if len(my_history) == 0:
         return 'c'
     if my_history[-1] == 'b' and their_history[-1] = 'c':
-        return 'b'
+        return 'c'
     elif my_score > -250:
         return 'c'
     elif their_score < -500
